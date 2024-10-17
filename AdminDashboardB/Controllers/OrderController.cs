6da582a3b600fdcab98651a539356d6c6ffe9791
KhaledAshrafMohammed <109352509+KhaledAshrafMohammed@users.noreply.github.com>
2024-10-17 20:13:55 +0300

    order layout

--- AdminDashboardB/Controllers/OrderController.cs
+++ AdminDashboardB/Controllers/OrderController.cs
@@ -4,5 +4,24 @@ namespace AdminDashboardB.Controllers
 {
     public class OrderController : Controller
     {
+
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+
+
+        public async Task<IActionResult> Edit()
+        {
+
+            return View();
+        }
+
+        //[HttpPost]
+        //public async Task<IActionResult> Edit( )
+        //{
+
+        //    return RedirectToAction(nameof(Index));
+        //}
     }
 }
