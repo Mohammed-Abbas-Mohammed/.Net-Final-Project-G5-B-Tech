ba500208dfa9d23c95efe44d38e4ae00a41f9e5b
Omar Raafat <oraafat03@gimal.com>
2024-10-27 12:25:59 +0300

    editname

--- B-Tech.API/Program.cs
+++ B-Tech.API/Program.cs
@@ -1,28 +1,17 @@
-using ApplicationB.Contracts_B.Product;
-using ApplicationB.Contracts_B;
-using ApplicationB.Mapper_B;
-using ApplicationB.Services_B.Product;
 using DbContextB;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Localization;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Options;
+using Microsoft.Identity.Abstractions;
+using Microsoft.Identity.Web;
+using Microsoft.Identity.Web.Resource;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using ModelsB.Authentication_and_Authorization_B;
-using System.Globalization;
-using InfrastructureB.Product;
-using ApplicationB.Services_B;
-using ApplicationB.Contracts_B.General;
-using ApplicationB.Services_B.General;
-using InfrastructureB.General;
-using ApplicationB.Contracts_B.Category;
-using ApplicationB.Services_B.Category;
-using InfrastructureB.Category;
-using AutoMapper;
-using ApplicationB.Contracts_B.Order;
-using ApplicationB.Services_B.Order;
-using InfrastructureB.Order;
+using System.Text;
 
-namespace B_Tech.API
+namespace B_TechApi
 {
     public class Program
     {
@@ -30,104 +19,93 @@ namespace B_Tech.API
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
             builder.Services.AddDbContext<BTechDbContext>(options =>
-               options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-            builder.Services.AddIdentity<ApplicationUserB, IdentityRole>()
-                   .AddEntityFrameworkStores<BTechDbContext>()
-                   .AddDefaultTokenProviders();
-
-            builder.Services.AddAutoMapper(typeof(MappingProfile));
-            builder.Services.AddHttpContextAccessor();
-
-            #region AddScoped
-            builder.Services.AddScoped<ILanguageRepository, LanguageRepository>();
-            builder.Services.AddScoped<ILanguageService, LanguageService>();
-            builder.Services.AddScoped<IUserService, UserService>();
-
-            //==========Product==========
-            builder.Services.AddScoped<IProductRepository, ProductRepository>();
-            builder.Services.AddScoped<IProductService, ProductService>();
-            builder.Services.AddScoped<IProductImageRepository, ProductImageRepository>();
-            builder.Services.AddScoped<IProductImageService, ProductImageService>();
-
-            builder.Services.AddScoped<IProductSpecificationRepository, ProductSpecificationRepository>();
-            builder.Services.AddScoped<IProductSpecificationService, ProductSpecificationService>();
-            builder.Services.AddScoped<IProductTranslationRepository, ProductTranslationRepository>();
-            builder.Services.AddScoped<IProductTranslationService, ProductTranslationService>();
-            builder.Services.AddScoped<IProductSpecificationTranslationRepository, ProductSpecificationTranslationRep>();
-            builder.Services.AddScoped<IProductSpecificationTransService, ProductSpecificationTransService>();
-            builder.Services.AddScoped<ISpecificationStoreRepository, SpecificationStoreRepository>();
-            builder.Services.AddScoped<ISpecificationStoreService, SpecificationStoreService>();
-
-            builder.Services.AddScoped<IReviewRepository, ReviewRepository>();
-            builder.Services.AddScoped<IReviewService, ReviewService>();
-
-            //==========Category==========
-            builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
-            builder.Services.AddScoped<ICategoryService, CategoryService>();
-
-            //===========Order==============
-            builder.Services.AddScoped<IOrderRepository, OrderRepository>();
-            builder.Services.AddScoped<IOrderService, OrderService>();
-
-            builder.Services.AddScoped<IOrderItemRepository, OrderItemRepository>();
-            builder.Services.AddScoped<IOrderItemService, OrderItemService>();
-
-            builder.Services.AddScoped<IShippingRepository, ShippingRepository>();
-            builder.Services.AddScoped<IShippingService, ShippingService>();
-
-            builder.Services.AddScoped<IPaymentRepository, PaymentRepository>();
-            builder.Services.AddScoped<PaymentService>();
-            #endregion
-
-            builder.Services.Configure<RequestLocalizationOptions>(options =>
+            builder.Services.AddAuthentication(options =>
             {
-                var supportedCultures = new[]
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(op =>
+            {
+                op.TokenValidationParameters = new TokenValidationParameters()
                 {
-                    new CultureInfo("en-US"),
-                    new CultureInfo("ar-EG")
+                    ValidateIssuer = true,
+                    ValidIssuer = builder.Configuration["jwt:Issuer"],
+                    ValidateAudience = true,
+                    ValidAudience = builder.Configuration["jwt:Audience"],
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["jwt:Key"]))
                 };
-
-                options.DefaultRequestCulture = new RequestCulture(culture: "en-US", uiCulture: "en-US");
-                options.SupportedCultures = supportedCultures;
-                options.SupportedUICultures = supportedCultures;
             });
 
-            // Add controllers
+            builder.Services.AddIdentity<ApplicationUserB, IdentityRole>()
+                .AddEntityFrameworkStores<BTechDbContext>().AddRoles<IdentityRole>();
+
+            //.AddRoles<ApplicationUserB>();
+
+
+            builder.Services.AddCors(op =>
+            {
+                op.AddPolicy("Default", policy =>
+                {
+                    policy.AllowAnyHeader()
+                          .AllowAnyOrigin()
+                          .AllowAnyMethod();
+                });
+            });
+
+
+
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen(c => {
+                c.SwaggerDoc(
+                    "v1", new OpenApiInfo
+                    {
+                        Title = "JWTToken_Auth_API",
+                        Version = "v1"
+                    });
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 1safsfsdfdfd\"",
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+        {
+            new OpenApiSecurityScheme {
+                Reference = new OpenApiReference {
+                    Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                }
+            },
+            new string[] {}
+        }
+    });
+            });
+
             builder.Services.AddControllers();
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
+            // Configure the HTTP request pipeline
             if (app.Environment.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                app.UseHsts();
+                app.UseSwagger();
+                app.UseSwaggerUI();
             }
 
             app.UseHttpsRedirection();
+            app.UseCors("Default");
+            app.UseAuthentication(); // ÇÓÊÎÏÇã ÇáãÕÇÏÞÉ
+            app.UseAuthorization();  // ÇÓÊÎÏÇã ÇáÊÝæíÖ
 
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers(); // This should be here to map API controller routes
-            });
-            var locOptions = app.Services.GetService<IOptions<RequestLocalizationOptions>>();
-            app.UseRequestLocalization(locOptions.Value);
-
-            app.UseAuthentication();
-            app.UseAuthorization();
-
-            // Map API routes
             app.MapControllers();
-
 
             app.Run();
         }
